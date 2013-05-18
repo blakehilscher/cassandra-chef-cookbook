db5a7effa64f2a4e64077345e459494aa4da5e4e
Blake Hilscher <blake@hilscher.ca>
2013-05-18 18:22:58 -0400

    add topology for managing via propertyfilesnitch

--- recipes/topology.rb
+++ recipes/topology.rb
@@ -3,7 +3,7 @@ if node.cassandra.snitch == 'PropertyFileSnitch'
   cass_nodes = search(:node, "role:quandl_cassandra AND chef_environment:#{node.chef_environment}") || []
   cass_nodes = cass_nodes.group_by{|n| n.ec2.placement_availability_zone }
   cass_nodes = cass_nodes.collect do |zone, cnodes|
-    cnodes.collect do |cnode|
+    cnodes.sort_by{|cn| cn.name }.collect do |cnode|
       # get the datacenter
       dc = node.cassandra.topology.datacenters[zone].to_i
       # get the rack
@@ -19,10 +19,9 @@ if node.cassandra.snitch == 'PropertyFileSnitch'
     mode  0644
     variables cass_nodes: cass_nodes
   end
-
+  
   service "cassandra" do
-    supports :restart => true, :status => true
-    action [:enable, :start]
+    action :restart
   end
   
 end
