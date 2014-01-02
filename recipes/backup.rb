8abab7ba4d965026201552d197a5608a733a831c
Blake Hilscher <blake@hilscher.ca>
2014-01-02 17:37:28 -0500

    executable

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -2,5 +2,5 @@ template File.join(node["cassandra"]["conf_dir"], "backup.sh") do
   source "backup.sh.erb"
   owner node["cassandra"]["user"]
   group node["cassandra"]["user"]
-  mode  0644
+  mode  0744
 end
